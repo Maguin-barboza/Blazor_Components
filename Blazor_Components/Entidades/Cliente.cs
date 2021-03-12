b2b4f65df39af9fd2ad597c5c7e7592f615b203c
Maguin-barboza <magno_barboza@hotmail.com>
2021-03-12 16:17:12 -0300

    Add project files.

---
+++ Blazor_Components/Entidades/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Components.Entidades
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public static List<Cliente> clientes = new List<Cliente>();
+
+        public void Inserir()
+        {
+            //TODO: Terminar Inserir
+            this.Id = Cliente.clientes.Count + 1;
+            Cliente.clientes.Add(this);
+        }
+
+        public void Remover()
+        {
+            //TODO: Terminar Remover
+            Cliente.clientes.Remove(this);
+        }
+    }
+}
---
+++ Blazor_Components/Entidades/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Components.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descrição { get; set; }
+        public decimal Preço { get; set; }
+
+        public static List<Produto> Produtos = new List<Produto>();
+
+        public void Inserir()
+        {
+            this.Id = Produto.Produtos.Count + 1;
+            Produto.Produtos.Add(this);
+        }
+
+        public void Remover()
+        {
+            Produto.Produtos.Remove(this);
+        }
+    }
+}
